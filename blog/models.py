1089436fd32e3de281b2c6e7085688a4bd972526
Deovrat Singh <devsingh6n@gmail.com>
2019-09-02 18:56:18 +0530

    Project portfolio completed

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     pub_date=models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     body=models.TextField()
+
+
+    def __str__(self):
+        return self.title
+
+
+
+    def summary(self):
+        return self.body[0:100]
--- blog/urls.py
+++ blog/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-import jobs.views
+
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('home/',jobs.views.home, name='home'),
-    path('blog/',include('blog.urls')),
-] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+    path('',views.allblogs, name='allblogs'),
+    path('<int:blog_id>/',views.detail,name='detail')
+
+]
